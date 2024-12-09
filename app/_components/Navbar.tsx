8197f8cd190a486473164b81bffc680f5c1a9607
wilson anjos <maxwilson465@gmail.com>
2024-12-08 22:14:30 -0300

    feat: menu activity

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -4,19 +4,34 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import Image from "next/image";
 import { UserMenu } from "./userMenu";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
   const { data: session } = useSession();
-
+  const pathname = usePathname();
   return (
     <nav className="flex items-center justify-between border-b border-solid px-4 py-4 md:px-8">
       <div className="flex items-center gap-4">
         <Image src="/logo.svg" width={173} height={39} alt="AI" />
 
-        <Link href="/" className="text-muted-foreground">
+        <Link
+          href="/"
+          className={
+            pathname === "/"
+              ? "font-bold text-primary"
+              : "text-muted-foreground"
+          }
+        >
           Dashboard
         </Link>
-        <Link href="/tasks" className="text-muted-foreground">
+        <Link
+          href="/tasks"
+          className={
+            pathname === "/tasks"
+              ? "font-bold text-primary"
+              : "text-muted-foreground"
+          }
+        >
           Tarefas
         </Link>
       </div>
